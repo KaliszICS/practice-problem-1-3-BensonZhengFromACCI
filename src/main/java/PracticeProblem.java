379234d95235cbad3f128cd80949166178d29fe0
BensonZhengFromACCI <343804126@tdsb.ca>
2026-02-12 16:35:47 +0000

    hey mr james kalish

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,5 +1,7 @@
 public class PracticeProblem {
 
+// Feb 12 2026, Benson Zheng
+// Coding shapes using new lines
 	public static void main(String args[]) {
 		q1();
 		q2();
@@ -8,19 +10,47 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println(
+						" * * * * * *\n" + 
+						"* * * * * *\n" + 
+						" * * * * * *\n" + 
+						"* * * * * *\n" + 
+						" * * * * * *\n" + 
+						"* * * * * *\n");
+		
+		System.out.println(		
+						"* * * * *\n" + 
+						"*       *\n" + 
+						"*       *\n" + 
+						"*       *\n" + 
+						"* * * * *\n");
+
+		System.out.println(
+						"* * * * * *\n" + 
+						" *       *\n" + 
+						"  *     *\n" + 
+						"   *   *\n" + 
+						"    * *\n" + 
+						"     *");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+	String frivolousWildAnimal = "          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''";
+	System.out.println(frivolousWildAnimal);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		String orphanBoyWithHiddenPowersAttendsSecretMagicSchoolThenDefeatsAncientDarkEvilSorcererToUltimatelySaveTheHiddenWizardingWorldAndRestorePeaceToTheMysticalReal = "Fortnite ";
+		String bad = "is awful";
+		System.out.println(orphanBoyWithHiddenPowersAttendsSecretMagicSchoolThenDefeatsAncientDarkEvilSorcererToUltimatelySaveTheHiddenWizardingWorldAndRestorePeaceToTheMysticalReal + bad);
+
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		String str1 = "Don't ";
+		String str2 = "@ me";
+		String str3 = str1 + str2;
+		System.out.println(str3);
 	}
 
 
